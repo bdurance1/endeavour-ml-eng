1bd3adc7569757256c83cfb65bd863ca1c4f6649
bdurance1 <bdurance1@gmail.com>
2022-09-22 15:44:28 +0000

    refactor:

--- infra/acm.tf
+++ infra/acm.tf
@@ -1,12 +1,12 @@
-# resource "aws_acm_certificate" "this" {
-#   domain_name       = local.domain_name
-#   validation_method = "DNS"
+resource "aws_acm_certificate" "this" {
+  domain_name       = local.domain_name
+  validation_method = "DNS"
 
-#   tags = {
-#     Name = "${local.namespace}-acm-endvservices"
-#   }
-# }
+  tags = {
+    Name = "${local.namespace}-acm-endvservices"
+  }
+}
 
-# resource "aws_acm_certificate_validation" "this" {
-#   certificate_arn = aws_acm_certificate.this.arn
-# }
+resource "aws_acm_certificate_validation" "this" {
+  certificate_arn = aws_acm_certificate.this.arn
+}
